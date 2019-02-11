8832079423adeee3e7bf98217cf51cccc4ac17aa
Gilberto Pucciarelli <gilbertopucciarelli@hotmail.com>
2019-02-11 19:16:12 -0400

    clases

---
+++ src/logica/Gerente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author Gilberto
+ */
+public class Gerente extends Thread{
+    
+    public Gerente(){
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    }
+    
+}
